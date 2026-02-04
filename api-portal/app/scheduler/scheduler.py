b20c595572b5ea5bd7f9ba8d47a7ad7866f6cf32
Marcel Meyer <marcel.meyer@uni-paderborn.de>
2026-02-04 12:18:28 +0100

    just a single score job

--- api-portal/app/scheduler/scheduler.py
+++ api-portal/app/scheduler/scheduler.py
@@ -271,6 +271,7 @@ class ChallengeScheduler:
                 id="periodic_challenge_scores_evaluation",
                 coalesce=CoalescePolicy.latest,
                 misfire_grace_time=300,  # 5 minute grace period
+                max_running_jobs=1,  # Only one instance at a time
             )
             self.logger.info(
                 "Scheduled periodic challenge scores evaluation job "
